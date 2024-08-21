2b0fdd2d95134a01fc0c647878d84cc5a64d6950
Andrew <wangsxin.xin@gmail.com>
2024-08-21 16:36:31 +1200

    update
    a

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,14 +1,81 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { AuthProvider } from "../../../backend/src/provider/AuthProvider";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import {db} from "../../../backend/src/firebaseSetup/firebaseSetup";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function App() {
+  const [newName, setNewName] = useState("");
+  const [newMarks, setNewMarks] = useState(0);
+
+  const [users, setUsers] = useState<any[]>([]);
+  const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, marks: Number(newMarks) });
+    getUsers();
+  };
+
+  const updateUser = async (id:any, marks:any) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { marks: marks + 1 };
+    await updateDoc(userDoc, newFields);
+    getUsers();
+  };
+
+  const deleteUser = async (id:any) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+    getUsers();
+  };
+
+  const getUsers = async () => {
+    const data = await getDocs(usersCollectionRef);
+    setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="App">
+    <div className="FormHolder">
+      <input
+        type="text"
+        placeholder="Name..."
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Marks..."
+        onChange={(event) => {
+          setNewMarks(Number(event.target.value));
+        }}
+      />
+      <button onClick={createUser}>Create User</button>
+      </div>
+      <div className="user-cards">
+        {users.map((user) => (
+          <div key={user.id} className="user-card">
+            <h1>Name: {user.name}</h1>
+            <h1>Marks: {user.marks}</h1>
+            <button onClick={() => updateUser(user.id, user.marks)}>
+              Increase Marks
+            </button>
+            <button onClick={() => deleteUser(user.id)}>Delete User</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
