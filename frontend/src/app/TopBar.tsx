e7dc14dd7f7d134741b42bb5c3ac44acac7acc47
Kevin <132238470+hch3745@users.noreply.github.com>
2024-08-29 12:59:11 +1200

    TopBar with notifications (phone and laptop ver)

---
+++ frontend/src/app/TopBar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import NotificationBell from './NotificationBell';
+import Image from "next/image";
+
+const TopBar: React.FC = () => {
+    return (
+        <div className="flex justify-between items-center p-4 bg-white relative z-40">
+            <div className="left-section">
+                <Image src="Logo.svg" alt="tripago logo" width={100} height={100} className="p-2" />
+            </div>
+            <div className="right-section">
+                <NotificationBell />
+            </div>
+        </div>
+    );
+};
+
+export default TopBar;
+
+/*<button className="notification-button">
+                    <i className="bell-icon">bell</i>
+                </button>
+                
+                
+
+import React from 'react';
+import NotificationBell from './NotificationBell';
+import Image from "next/image";
+
+const TopBar: React.FC = () => {
+    return (
+        <div className="flex flex-row justify-between items-center p-3">
+            <div className="left-section">
+                <Image src="Logo.svg" alt="tripago logo" width={100} height={100}/>
+            </div>
+            <div className="right-section">
+                <NotificationBell />
+            </div>
+        </div>
+    );
+};
+
+export default TopBar;                */
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
+import TopBar from "./TopBar";
 
 export default function Map() {
   const mapRef = React.useRef<HTMLDivElement>(null);
@@ -14,8 +15,6 @@ export default function Map() {
       });
 
       const { Map } = await loader.importLibrary("maps");
-      //init a marker
-      //const { Marker } = await loader.importLibrary('marker') as google.maps.MarkerLibrary;
 
       const position = {
         lat: -36.8484611,
@@ -31,15 +30,56 @@ export default function Map() {
 
       //setup the map
       const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
-
-      //put up marker
-      /*const marker = new Marker({
-        map: map,
-        position: position
-      });*/
     };
 
     initMap();
   }, []);
-  return <div style={{ height: "600px" }} ref={mapRef} />;
+  return (
+    <div className="flex flex-col h-screen">
+      <TopBar />
+      <div className="flex-grow z-0" ref={mapRef} />
+    </div>
+  );
 }
+
+/*"use client";
+import React, { useEffect } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+import TopBar from "./TopBar";
+
+export default function Map() {
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const initMap = async () => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+        version: "weekly",
+      });
+
+      const { Map } = await loader.importLibrary("maps");
+
+      const position = {
+        lat: -36.8484611,
+        lng: 174.7597086,
+      };
+
+      const mapOptions: google.maps.MapOptions = {
+        center: position,
+        zoom: 15,
+        mapId: "MY_MAPS_MAPID",
+      };
+
+      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+    };
+
+    initMap();
+  }, []);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <TopBar />
+      <div className="flex-grow" ref={mapRef} />
+    </div>
+  );
+} */
