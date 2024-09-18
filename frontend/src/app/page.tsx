b6abf2bddfc01a1aff82cf860713521310ee570c
Kevin <132238470+hch3745@users.noreply.github.com>
2024-09-19 01:13:06 +1200

    near

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -12,23 +12,26 @@ if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function Home() {
-  const amount = 49.99;
+  const amount = 0.99;
 
   return (
-    <main className="max-w-6xl mx-auto p-10 text-whte text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-blue-500">
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">Tripago</h1>
-        <h2 className="text-2xl">has requested
-        <span className="font-bold"> ${amount}</span>
+        <h2 className="text-2xl">
+          has requested
+          <span className="font-bold"> ${amount}</span>
         </h2>
       </div>
 
-      <Elements stripe={stripePromise} options={{
-        mode: "payment", 
-        amount: convertToSubcurrency(amount), 
-        currency: "nzd", 
-      }}>
-        <CheckoutPage amount={amount}/>
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: "payment",
+          amount: convertToSubcurrency(amount),
+          currency: "nzd",
+        }}>
+        <CheckoutPage amount={amount} />
       </Elements>
     </main>
   );
