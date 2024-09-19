6ba52cdf2f13b8ef37d603334e51e0c4f8e4f7f0
Kevin <132238470+hch3745@users.noreply.github.com>
2024-09-19 14:48:24 +1200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/payment_success/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/payment_success/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
