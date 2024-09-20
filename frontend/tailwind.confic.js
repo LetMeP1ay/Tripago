dd706e93591bbff5b8a7e258d1bc3dc2a3d3afa0
Kevin <132238470+hch3745@users.noreply.github.com>
2024-09-20 16:25:15 +1200

    Fix

--- frontend/tailwind.confic.js
+++ frontend/tailwind.confic.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages//.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/.{js,ts,jsx,tsx,mdx}",
-    "./src/app//*.{js,ts,jsx,tsx,mdx}",
-    "./components//.{js,ts,jsx,tsx,mdx}",
-    "./app/payment_success/**/.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",  
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./app/payment_success/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
-    exten1d: {},
+    extend: {},
   },
   plugins: [],
-}
+};
