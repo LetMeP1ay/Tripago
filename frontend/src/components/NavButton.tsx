0b522f50cb9bfd957060cccc1b957a8e7c5e5169
LetMeP1ay <vdaberezin@gmail.com>
2024-10-10 11:57:56 +1300

    edit the header

--- frontend/src/components/NavButton.tsx
+++ frontend/src/components/NavButton.tsx
@@ -7,12 +7,14 @@ interface NavButtonProps {
   label: string;
   isActive: boolean;
   onClick: () => void;
+  type?: "button" | "text";
 }
 
 export default function NavButton({
   label,
   isActive,
   onClick,
+  type,
 }: NavButtonProps) {
   const router = useRouter();
 
@@ -25,22 +27,41 @@ export default function NavButton({
     ? `/${label.toLowerCase()}Active.svg`
     : `/${label.toLowerCase()}.svg`;
 
-  return (
-    <button
-      className={
-        `w-full flex items-center justify-center rounded-lg p-3.5 font-medium lg:text-2xl transition-all duration-600 ease-in-out ` +
-        (isActive ? `text-white bg-[#71D1FC] hover:bg-[#5BBEEB] active:bg-[#5AAEEA]` : `text-[#727272] bg-[#DADADA] hover:bg-[#CCCCCC] active:bg-[#BBB]`)
-      }
-      onClick={handleClick}
-    >
-      <Image
-        alt={`${label} Icon`}
-        src={iconSrc}
-        width={0}
-        height={0}
-        className="w-6 md:w-8"
-      />
-      <p className="pl-4">{label}</p>
-    </button>
-  );
+  switch (type) {
+    case "button":
+      return (
+        <button
+          className={
+            `w-full flex items-center justify-center rounded-lg p-3.5 font-medium transition-all duration-600 ease-in-out ` +
+            (isActive
+              ? `text-white bg-[#71D1FC] hover:bg-[#5BBEEB] active:bg-[#5AAEEA]`
+              : `text-[#727272] bg-[#DADADA] hover:bg-[#CCCCCC] active:bg-[#BBB]`)
+          }
+          onClick={handleClick}
+        >
+          <Image
+            alt={`${label} Icon`}
+            src={iconSrc}
+            width={0}
+            height={0}
+            className="w-6"
+          />
+          <p className="pl-4">{label}</p>
+        </button>
+      );
+    case "text":
+      return (
+        <p
+          className={
+            `w-full font-medium text-2xl transition-all duration-600 ease-in-out cursor-pointer ` +
+            (isActive
+              ? `text-[#71D1FC] hover:text-[#5BBEEB] active:text-[#5AAEEA]`
+              : `text-[#BBB] hover:text-[#AAA] active:text-[#A0A0A0]`)
+          }
+          onClick={handleClick}
+        >
+          {label}
+        </p>
+      );
+  }
 }
