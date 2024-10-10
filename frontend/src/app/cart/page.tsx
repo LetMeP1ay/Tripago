0ac26079ed0e8b335b978461f57d35fae9478b79
LetMeP1ay <vdaberezin@gmail.com>
2024-10-11 00:28:44 +1300

    apply prettier to currencyConversion. Remove the buttons from rendering on the cart page

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,8 +1,7 @@
 export default function ShoppingCart() {
-
-    return(
-        <div className="flex w-full justify-center items-center text-black border-b border-[#EEF0EB] pb-4 text-2xl">
-            <p>Order Details</p>
-        </div>
-    )
+  return (
+    <div className="flex w-full justify-center items-center text-black border-b border-[#EEF0EB] pb-4 md:py-6 md:text-xl lg:text-2xl">
+      <p>Order Details</p>
+    </div>
+  );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -28,11 +28,12 @@ function useWindowWidth() {
 export default function Header() {
   const pathname = usePathname();
   const router = useRouter();
-  const [activeButton, setActiveButton] = useState<string>("");
-  const [loadingScreen, setLoadingScreen] = useState<boolean>(true);
-
   const windowWidth = useWindowWidth();
   const isDesktop = windowWidth >= 768;
+
+  const [activeButton, setActiveButton] = useState<string>("");
+  const [screenLoaded, setScreenLoaded] = useState<boolean>(false);
+  const [carouselScreen, setCarouselScreen] = useState<boolean>(true);
 
   useEffect(() => {
     if (pathname.includes("/flights")) {
@@ -55,92 +56,95 @@ export default function Header() {
   }, [pathname]);
 
   function displayViewSetter() {
-    setLoadingScreen(false);
+    setScreenLoaded(true);
   }
 
   useEffect(() => {
-    setTimeout(displayViewSetter, 2000);
+    setTimeout(displayViewSetter, 800);
   }, []);
 
-  if (loadingScreen && isDesktop) {
+  if (screenLoaded) {
     return (
-      <div className="flex h-screen w-screen items-center justify-center">
-        <div
-          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-black"
-          role="status"
-        >
-          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-            Loading...
-          </span>
-        </div>
-      </div>
+      <header className="w-full md:bg-[#474646]">
+        <nav className="p-4 flex flex-col md:flex-row md:justify-between md:items-center">
+          <div className="flex justify-between items-center w-full md:w-auto">
+            <Image
+              src="/Tripago.svg"
+              alt="Tripago Logo"
+              width={0}
+              height={0}
+              className="w-24 hover:cursor-pointer"
+              onClick={() => router.push("/")}
+            />
+            {!isDesktop && <NotificationBell />}
+          </div>
+          {!pathname.includes("/cart") ||
+            (isDesktop && (
+              <div className="flex justify-center items-center space-x-3 md:space-x-8 mt-8 md:mt-0 md:ml-auto md:mr-8">
+                <NavButton
+                  type={isDesktop ? "text" : "button"}
+                  label="Hotels"
+                  isActive={activeButton === "Hotels"}
+                  onClick={() => router.push("/hotels")}
+                />
+                <NavButton
+                  type={isDesktop ? "text" : "button"}
+                  label="Flights"
+                  isActive={activeButton === "Flights"}
+                  onClick={() => router.push("/flights")}
+                />
+                <NavButton
+                  type={isDesktop ? "text" : "button"}
+                  label="Food"
+                  isActive={activeButton === "Food"}
+                  onClick={() => router.push("/food")}
+                />
+              </div>
+            ))}
+          {isDesktop && (
+            <div className="flex items-center space-x-8">
+              <NavButton
+                type="text"
+                label="Map"
+                isActive={activeButton === "Map"}
+                onClick={() => router.push("/")}
+              />
+              <NavButton
+                type="text"
+                label="Favorites"
+                isActive={activeButton === "Favorites"}
+                onClick={() => router.push("/favorites")}
+              />
+              <NavButton
+                type="text"
+                label="Cart"
+                isActive={activeButton === "Cart"}
+                onClick={() => router.push("/cart")}
+              />
+              <NavButton
+                type="text"
+                label="Profile"
+                isActive={activeButton === "Profile"}
+                onClick={() => router.push("/profile")}
+              />
+              <NotificationBell />
+            </div>
+          )}
+        </nav>
+      </header>
     );
   }
 
   return (
-    <header className="w-full md:bg-[#474646]">
-      <nav className="p-4 flex flex-col md:flex-row md:justify-between md:items-center">
-        <div className="flex justify-between items-center w-full md:w-auto">
-          <Image
-            src="/Tripago.svg"
-            alt="Tripago Logo"
-            width={0}
-            height={0}
-            className="w-24 hover:cursor-pointer"
-            onClick={() => router.push("/")}
-          />
-          {!isDesktop && <NotificationBell />}
-        </div>
-        <div className="flex justify-center items-center space-x-3 md:space-x-8 mt-8 md:mt-0 md:ml-auto md:mr-8">
-          <NavButton
-            type={isDesktop ? "text" : "button"}
-            label="Hotels"
-            isActive={activeButton === "Hotels"}
-            onClick={() => router.push("/hotels")}
-          />
-          <NavButton
-            type={isDesktop ? "text" : "button"}
-            label="Flights"
-            isActive={activeButton === "Flights"}
-            onClick={() => router.push("/flights")}
-          />
-          <NavButton
-            type={isDesktop ? "text" : "button"}
-            label="Food"
-            isActive={activeButton === "Food"}
-            onClick={() => router.push("/food")}
-          />
-        </div>
-        {isDesktop && (
-          <div className="flex items-center space-x-8">
-            <NavButton
-              type="text"
-              label="Map"
-              isActive={activeButton === "Map"}
-              onClick={() => router.push("/")}
-            />
-            <NavButton
-              type="text"
-              label="Favorites"
-              isActive={activeButton === "Favorites"}
-              onClick={() => router.push("/favorites")}
-            />
-            <NavButton
-              type="text"
-              label="Cart"
-              isActive={activeButton === "Cart"}
-              onClick={() => router.push("/cart")}
-            />
-            <NavButton
-              type="text"
-              label="Profile"
-              isActive={activeButton === "Profile"}
-              onClick={() => router.push("/profile")}
-            />
-            <NotificationBell />
-          </div>
-        )}
-      </nav>
-    </header>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-black"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    </div>
   );
 }
