16bcd1127a0a59a4222455a9f9bef543543b243f
Kevin <132238470+hch3745@users.noreply.github.com>
2024-10-11 15:53:51 +1300

    rebase onto latest branch

--- frontend/src/app/payment_success/page.tsx
+++ frontend/src/app/payment_success/page.tsx
@@ -1,18 +1,50 @@
+'use client'
+
+import React, { useState } from 'react';
+import Image from "next/image";
+
 export default function PaymentSuccess({
   searchParams: { amount },
 }: {
   searchParams: { amount: string };
 }) {
+  const [currentDate, setCurrentDate] = useState(getDate());
+  const [currentTime, setCurrentTime] = useState(getTime());
   return (
-    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-blue-500">
-      <div className="mb-10">
+    <div>
+      <div className="text-black text-center p-5 text-4xl font-bold">Payment Status</div>
+      <div className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-blue-500">
+        {/*<div className="">*/}
         <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-        <h2 className="text-2xl">Payment Successful</h2>
+        <div className="flex justify-center items-center">
+          <Image src="ic-success.svg" alt="tick" width={200} height={200}></Image>
+        </div>
+        <h2 className="text-2xl font-bold mb-2">Payment Success</h2>
+        <p className="text-xl font-bold mb-2">{currentDate} {currentTime}</p>
 
         <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
           ${amount}
         </div>
+        <button onClick={() => { window.location.href = url; }} className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse">Return Home</button>
+        {/*</div>*/}
       </div>
-    </main>
+    </div>
   );
+}
+
+let url = `http://localhost:3000`
+
+function getDate() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${date}/${month}/${year}`;
+}
+
+function getTime() {
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  return `${hours}:${minutes}`
 }
--- frontend/src/app/stripe.tsx
+++ frontend/src/app/stripe.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from "react";
+import React, { useState } from "react";
 import CheckoutPage from "@/components/CheckoutPage";
 import convertToSubcurrency from "@/../lib/convertToSubcurrency";
 import { Elements } from "@stripe/react-stripe-js";
@@ -13,28 +13,54 @@ if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function Stripe() {
-  const amount = 0.99;
+  const amount = 750;
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
 
   return (
-    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-blue-500">
-      <div className="mb-10">
-        <h1 className="text-4xl font-extrabold mb-2">Tripago</h1>
-        <h2 className="text-2xl">
-          has requested
-          <span className="font-bold"> ${amount}</span>
-        </h2>
-      </div>
+    <div>
+      <div className="text-center text-black text-4xl font-bold p-5">Payment</div>
+      <div className="max-w-6xl mx-auto p-10 text-white border m-10 rounded-md bg-blue-500">
+        <div className="mb-10">
+          <h1 className="text-4xl font-extrabold mb-2 text-center">Tripago</h1>
+          <div className="flex justify-between items-center">
+            <button className="text-white text-2xl" onClick={toggleDetails}>
+              View Details</button>
+            <div>
+              <span className="text-2xl">Total: </span>
+              <span className="text-2xl font-bold">${amount}</span>
+            </div>
+          </div>
 
-      <Elements
-        stripe={stripePromise}
-        options={{
-          mode: "payment",
-          amount: convertToSubcurrency(amount),
-          currency: "nzd",
-        }}
-      >
-        <CheckoutPage amount={amount} />
-      </Elements>
-    </main>
+          {/* Animated dropdown */}
+          <div
+            className={`transition-all duration-500 ease-in-out overflow-hidden ${
+              showDetails ? 'max-h-96' : 'max-h-0'
+            }`}>
+            <div className="bg-white text-black mt-5 p-5 rounded-lg">
+              <h3 className="text-2xl font-bold mb-3">Order Details</h3>
+              <ul className="list-disc list-inside">
+                <li>Plane Ticket - Auckland to Sydney - $300</li>
+                <li>Hotel Booking - 3 Nights in Sydney - $450</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <Elements
+          stripe={stripePromise}
+          options={{
+            mode: "payment",
+            amount: convertToSubcurrency(amount),
+            currency: "nzd",
+          }}>
+
+          <CheckoutPage amount={amount} />
+        </Elements>
+      </div>
+    </div>
   );
 }
