8ecb90ef45cabc720abcc085873a3e6922c748ec
LetMeP1ay <vdaberezin@gmail.com>
2024-10-11 17:26:47 +1300

    WIP - frontend work

--- frontend/src/components/HotelButton.tsx
+++ frontend/src/components/HotelButton.tsx
@@ -9,7 +9,7 @@ export default function HotelButton({ label, isActive }: HotelButtonProprs) {
   return (
     <button
       className={
-        `w-40 md:w-full mr-4 h-8 md:h-10 md:mb-6 rounded-[50px] md:rounded-md justify-center items-center flex p-3.5 font-medium lg:text-2xl transition-all duration-600 ease-in-out ` +
+        `w-40 md:w-full mr-4 h-8 md:h-10 md:mb-6 rounded-[50px] md:rounded-md justify-center items-center flex p-3.5 font-medium transition-all duration-600 ease-in-out ` +
         (isActive
           ? `text-white bg-[#71D1FC] hover:bg-[#5BBEEB] active:bg-[#5AAEEA]`
           : `text-[#727272] bg-[#ebebeb]  hover:bg-[#DADADA] active:bg-[#BBB]`)
