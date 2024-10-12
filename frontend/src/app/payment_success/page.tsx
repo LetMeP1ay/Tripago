1052bf388e810f07888846217ac34a6db196ea66
Kevin <132238470+hch3745@users.noreply.github.com>
2024-10-12 22:53:11 +1300

    Update page.tsx

--- frontend/src/app/payment_success/page.tsx
+++ frontend/src/app/payment_success/page.tsx
@@ -3,7 +3,6 @@ export default function PaymentSuccess({
 }: {
   searchParams: { amount: string };
 }) {
-  let url = `http://localhost:3000`;
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-blue-500">
       <div className="mb-10">
@@ -13,7 +12,6 @@ export default function PaymentSuccess({
         <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
           ${amount}
         </div>
-        <button onClick={() => { window.location.href = url; } } className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse">Return Home</button>
       </div>
     </main>
   );
