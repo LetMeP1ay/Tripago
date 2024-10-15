0ff6e6cb09f2405077565a20490e70b2976719cc
LetMeP1ay <vdaberezin@gmail.com>
2024-10-15 22:04:10 +1300

    Rebase on latest branch

--- frontend/src/app/stripe.tsx
+++ frontend/src/app/stripe.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
-import CheckoutPage from "@/components/CheckoutPage";
 import convertToSubcurrency from "@/../lib/convertToSubcurrency";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import CheckoutPage from "@/components/CheckoutPage";
 
 if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
   throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
@@ -22,19 +22,26 @@ export default function Stripe() {
 
   return (
     <div>
-      <div className="text-center text-black text-4xl font-bold p-5">Payment</div>
+      <div className="text-center text-black text-4xl font-bold p-5">
+        Payment
+      </div>
       <div className="max-w-6xl mx-auto p-10 text-white border m-10 rounded-md bg-blue-500">
         <div className="mb-10">
           <h1 className="text-4xl font-extrabold mb-2 text-center">Tripago</h1>
           <div className="flex justify-between items-center">
             <button className="text-white text-2xl" onClick={toggleDetails}>
-              View Details</button>
+              View Details
+            </button>
             <div>
               <span className="text-2xl">Total: </span>
               <span className="text-2xl font-bold">${amount}</span>
             </div>
           </div>
-          <div className={`transition-all duration-500 ease-in-out overflow-hidden ${showDetails ? 'max-h-96' : 'max-h-0'}`}>
+          <div
+            className={`transition-all duration-500 ease-in-out overflow-hidden ${
+              showDetails ? "max-h-96" : "max-h-0"
+            }`}
+          >
             <div className="bg-white text-black mt-5 p-5 rounded-lg">
               <h3 className="text-2xl font-bold mb-3">Order Details</h3>
               <ul className="list-disc list-inside">
@@ -51,8 +58,8 @@ export default function Stripe() {
             mode: "payment",
             amount: convertToSubcurrency(amount),
             currency: "nzd",
-          }}>
-
+          }}
+        >
           <CheckoutPage amount={amount} />
         </Elements>
       </div>
