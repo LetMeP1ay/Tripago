7d2a77c9042c50d663df829660ea85a3907dc3af
LetMeP1ay <vdaberezin@gmail.com>
2024-10-23 15:05:51 +1300

    remove unused code

--- frontend/src/app/food/page.tsx
+++ frontend/src/app/food/page.tsx
@@ -267,19 +267,6 @@ export default function FindFood() {
         </div>
       )}
       {loading && <p className="pt-60">Finding your next meal...</p>}
-      {!latitude ||
-        (!longitude && (
-          <div className="flex h-screen w-screen items-center justify-center">
-            <div
-              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-black"
-              role="status"
-            >
-              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                Loading...
-              </span>
-            </div>
-          </div>
-        ))}
     </div>
   );
 }
